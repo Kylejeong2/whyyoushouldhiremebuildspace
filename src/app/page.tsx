86b23c9a278241772204a419d91c10f85a0cd1c2
Kylejeong2 <Kylejeong21@gmail.com>
2024-07-17 16:04:53 -0700

    fixed videos + added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,10 @@ import {
 import ImageModal from '@/components/ImageModal';
 import { useRef, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
+import { Loader } from '@googlemaps/js-api-loader';
 
 export default function Home() {
+  // for image popup
   const [modalImage, setModalImage] = React.useState<string | null>(null);
 
   const openModal = (src: string) => setModalImage(src);
@@ -55,50 +57,63 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col 2xl:flex-row h-screen items-center justify-between text-white font-manrope bg-black font-extrabold tracking-tighter">
-      <div className="h-full w-full 2xl:w-1/4 bg-black text-white p-8 flex flex-col justify-between">
+    <main className="flex flex-col lg:flex-row h-screen items-center justify-between text-white font-manrope bg-black font-extrabold tracking-tighter">
+      <div className="h-full w-full 2xl:w-1/4 xl:w-1/3 bg-black items-center text-white p-8 flex flex-col justify-between">
         <div className="hidden lg:block">
-            picture 
+            <Image 
+              src="/buildspace-kyle.png" 
+              alt="Pic of Kyle with buildspace hat" 
+              width={300} 
+              height={300} 
+              className="rounded-full aspect-square object-cover shadow-lg" 
+            />
           </div>
         <div className="mt-auto">
-          <h1 className="text-5xl 2xl:text-7xl font-bold mb-4">hey there, i'm kyle.</h1>
+          <h1 className="text-7xl font-bold mb-4">hey there, i'm kyle.</h1>
           <p className="text-gray-400 text-lg font-medium 2xl:text-xl">this is why you should hire me as a buildspace intern (ai or full-stack) this fall!</p>
           <div className="py-4 flex justify-center space-x-4">
-            <Button variant="outline" className="font-bold h-16 text-lg w-auto rounded-none text-black hover:bg-black hover:text-white transition-colors duration-200">
+            <Button 
+              onClick={() => window.open('https://github.com/Kylejeong2/whyyoushouldhiremebuildspace', '_blank')}
+              variant="outline" 
+              className="font-bold h-16 text-lg w-1/2 rounded-none text-black hover:bg-black hover:text-white transition-colors duration-200"
+            >
               github repo for this site
             </Button>
-            <Button className="h-16 w-auto font-bold bg-black text-lg rounded-none text-white border-solid border-white border-2 hover:bg-white hover:text-black transition-colors duration-200">
-              cue background music
+            <Button 
+              onClick={() => window.open('https://twitter.com/kylejeong21', '_blank')}
+              className="h-16 w-1/2 font-bold bg-black text-lg rounded-none text-white border-solid border-white border-2 hover:bg-white hover:text-black transition-colors duration-200"
+            >
+              my twitter
             </Button>
           </div>
         </div>
       </div>
-      <div className="w-full 2xl:w-3/4 h-screen overflow-y-auto p-4 2xl:p-8 bg-gray-950 border-l-2 border-l-gray-800">
+      <div className="w-full 2xl:w-3/4 xl:w-2/3 h-screen overflow-y-auto p-4 2xl:p-8 bg-gray-950 border-l-2 border-l-gray-800">
 
       <div className="space-y-8 text-center p-20">
         <div className="mb-4">
           <h1 className="text-6xl font-bold p-6">i love building!</h1>
-          <p className="text-gray-400 text-xl">building is my passion, whether it be legos, computers, programming projects, or even muscles and it's something that i'll do for the rest of my life. </p>
+          <p className="text-gray-400 text-xl">whether it be legos, computers, or programming projects; i love building stuff and it's something that i'll do for the rest of my life. </p>
         </div>
         <div className="flex justify-center">
           <Carousel className="w-full bg-gray-600 rounded-lg max-w-6xl">
             <CarouselContent>
-              <CarouselItem className="p-10">
-              <div className="mb-8 text-center font-extrabold text-xl">
-                this is me getting featured in the lego magazine after winning a building competition + me programming a lego robot to move around and fight aliens.
+              <CarouselItem className="p-8 justify-between">
+              <div className="mb-8 text-center justify-between font-extrabold text-xl">
+                my feature in the lego magazine after winning a building competition + me programming a lego robot to move around and fight aliens.
                 <div className="flex flex-row items-center justify-center space-x-4 mt-4">
                   <Image 
                     src="/legoMagazine.png" 
                     alt="Lego" 
                     width={350} 
-                    height={400} 
+                    height={350} 
                     className="rounded-lg shadow-lg cursor-pointer" 
                     onClick={() => openModal("/legoMagazine.png")}
                   />
                   <Image 
                     src="/robotics.jpg" 
                     alt="Lego" 
-                    width={600} 
+                    width={400} 
                     height={300} 
                     className="rounded-lg shadow-lg cursor-pointer" 
                     onClick={() => openModal("/robotics.jpg")}
@@ -106,57 +121,56 @@ export default function Home() {
                 </div>
               </div>
               </CarouselItem>
-              <CarouselItem className="p-10">
-              <div className="mb-8 text-center font-extrabold text-xl">
+              <CarouselItem className="p-8 justify-between">
+              <div className="mb-8 text-center justify-between font-extrabold text-xl">
                 here are some pc computers I've built:
                   <div className="flex flex-row items-center justify-center space-x-4 mt-4">
                     <Image 
                       src="/pc1.jpeg" 
                       alt="PC Build 1" 
                       width={350} 
-                      height={300} 
+                      height={250} 
                       className="rounded-lg shadow-lg cursor-pointer" 
-                      onClick={() => openModal("/pc1.jpg")}
+                      onClick={() => openModal("/pc1.jpeg")}
                     />
                     <Image 
                       src="/pc2.jpeg" 
                       alt="PC Build 2" 
                       width={350} 
-                      height={300} 
+                      height={250} 
                       className="rounded-lg shadow-lg cursor-pointer" 
-                      onClick={() => openModal("/pc2.jpg")}
+                      onClick={() => openModal("/pc2.jpeg")}
                     />
                     <Image 
                       src="/pc3.jpeg" 
                       alt="PC Build 2" 
                       width={350} 
-                      height={300} 
-                      className="rounded-lg shadow-lg cursor-pointer" 
-                      onClick={() => openModal("/pc2.jpg")}
+                      height={250} 
+                      className="rounded-lg shadow-lg cursor-pointer lg:hidden" 
+                      onClick={() => openModal("/pc3.jpeg")}
                     />
                   </div>
                 </div>
               </CarouselItem>
-              <CarouselItem className="p-10">
-                <div className="mb-8 text-center font-extrabold text-xl">
-                  some cool projects i've shipped during n&w s5:
+              <CarouselItem className="p-8 justify-between">
+                <div className="mb-8 text-center justify-between font-extrabold text-xl">
+                  <h1>some cool projects i've shipped during n&w s5:</h1>
                   <div className="flex flex-row items-center justify-center space-x-4 mt-4">
-                    {/* change to github repo links */}
                     <Image 
-                      src="/project1.png" 
-                      alt="Project 1" 
-                      width={300} 
+                      src="/hackletter.png" 
+                      alt="HackLetter" 
+                      width={350} 
                       height={300} 
                       className="rounded-lg shadow-lg cursor-pointer" 
-                      onClick={() => openModal("/project1.png")}
+                      onClick={() => window.open('https://www.hackletter.co', '_blank')}                    
                     />
                     <Image 
-                      src="/project2.png" 
-                      alt="Project 2" 
-                      width={300} 
+                      src="/disciple.png" 
+                      alt="Disciple AI" 
+                      width={400} 
                       height={300} 
                       className="rounded-lg shadow-lg cursor-pointer" 
-                      onClick={() => openModal("/project2.png")}
+                      onClick={() => window.open('https://github.com/Kylejeong2/DiscipleAI', '_blank')}                    
                     />
                   </div>
                 </div>
@@ -190,31 +204,30 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="text-2xl mb-4 text-center">
-        <h1 className="text-6xl font-bold p-6">i'm relentless in my pursuit.</h1>
+      <div className="text-2xl text-center">
+        <h1 className="text-6xl font-bold p-6">i'm relentless in my pursuit (of literally everything).</h1>
         <p className="text-gray-400 text-2xl">if i want something done, i'll get it done</p>
-        <p className="text-gray-400 text-xl">here's clip of me wrestling through a tough point deficit + me fighting through a 425 squat.</p>
+        <p className="text-gray-400 text-xl">here's clip of me wrestling through a tough takedown (jiu jitsu jeff knows) + me fighting through a 425 squat.</p>
         <div className="flex justify-between mt-4">
           {['squat.mov', 'wrestling.mov'].map((src, index) => (
-            <div key={src} className="h-1/2 px-2 rounded-lg">
+            <div key={src} className="w-1/2 px-2 rounded-lg">
               <video
-                ref={(el) => { videoRefs.current[index] = el; }}
+                ref={(el) => { videoRefs.current[index + 3] = el; }}
                 src={src}
-                className="w-full h-auto cursor-pointer rounded-lg" 
+                className={`border-solid border-white border-2 w-full h-4/5 cursor-pointer rounded-lg ${src === 'wrestling.mov' ? 'object-cover' : ''}`}
                 loop
                 muted
                 playsInline
-                onClick={() => toggleMute(index)}
+                onClick={() => toggleMute(index + 3)}
               />
             </div>
           ))}
         </div>
-
       </div>
 
-      <div className="text-4xl mb-4 text-center">
-        i want to help build the future of AI and help other people build what they care about. 
-        <div className="text-gray-400 text-xl">buildspace n&w has had a huge affect on me these past weeks and i want to help spread it to everyone as much as possible</div>
+      <div className="text-6xl mb-8 text-center">
+        <h1 className="p-4">i want to help people build what they care about. </h1>
+        <div className="text-gray-400 text-xl">buildspace n&w has had a huge affect on me these past weeks and i want to help spread it to everyone.</div>
       </div>
 
       <div className="text-4xl mb-4 text-center">
@@ -228,15 +241,28 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="text-4xl flex flex-row items-center mb-4 text-center">
+      <Image 
+          src="/gmap.png" 
+          alt="map" 
+          width={700} 
+          height={400} 
+          className="rounded-lg shadow-lg cursor-pointer" 
+          onClick={() => openModal("/gmap.png")}
+        />
+        i'm also based near sf 📍(~40 mins away)
+      </div>
+
       <div className="flex flex-col items-center justify-center mb-8">
-        <h2 className="text-4xl font-bold mb-2">this is just a bonus, but farza already loves me:</h2>
+        <h2 className="text-4xl font-bold p-6 mb-2">this is just a bonus, but farza already loves me:</h2>
         <Tweet id="1808350569486270797"/>
       </div>
 
-      <div>My S5 journey behind the scenes + loom videos carousel. </div>
+      <div className="text-4xl mb-4 text-center">my s5 journey behind the scenes.</div>
+      
 
       <div className="text-2xl mb-4 text-center">
-        thanks to the Buildspace team for a great s5.
+        thanks to the buildspace team for a great s5.
 
         shoot confetti and play buildspace grant song
         <ul className="list-disc list-inside mt-2">
